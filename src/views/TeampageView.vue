d7541a132d13805ad4758ffeab19163288401634
김세희 <lolry0207@sookmyung.ac.kr>
2024-02-01 02:11:20 +0900

    Delete script set up tag

--- src/views/TeampageView.vue
+++ src/views/TeampageView.vue
@@ -23,8 +23,8 @@
 
                 <div class="end-bar">
                     <h5>프로젝트 마감까지</h5>
-                    <div id="bar"></div>
-                    <h5>D-{{ project.dDay }}</h5>
+                    <div id="bar" ref="bar"></div>
+                    <h5>D-{{ dDay }}</h5>
                 </div>
                 
                 <div class="btn-container">
@@ -41,7 +41,6 @@
                         :time-format-options="{ hour: 'numeric', minute: '2-digit' }"
                         :disable-past="state.disablePast"
                         :disable-future="state.disableFuture"
-                        :show-times="state.showTimes"
                         :date-classes="myDateClasses()"
                         :display-period-uom="state.displayPeriodUom"
                         :display-period-count="state.displayPeriodCount"
@@ -141,7 +140,7 @@
                         <h4>프로젝트명</h4>
                         </div>
                         <div>
-                        <input type="text" value="${project.exProjectName}" id="projectname" class="input-long"/>
+                        <input type="text" v-model="exProjectName" id="projectname" class="input-long" autocomplete='off'/>
                         </div>
                     </div>
 
@@ -150,7 +149,7 @@
                         <h4>팀명</h4>
                         </div>
                         <div>
-                        <input type="text" value="${project.exTeamName}" id="teamname" class="input-long"/>
+                        <input type="text" v-model="exTeamName" id="teamname" class="input-long" autocomplete='off'/>
                         </div>
                     </div>
 
@@ -532,7 +531,66 @@ export default {
     return {
         modalCheck: false,
         modalCheck2: false,
+        project: {
+            exProjectName: '개발 전용 웹 페이지 만들기리기리기억되리',
+            exTeamName: '스프링뷰트',
+            newProjectStartDate: '',
+            newProjectEndDate: '',
+            ProjectStartDate: '2024-01-02', // 프로젝트 시작일 데이터
+            ProjectEndDate: '2024-02-07', // 프로젝트 마감일 데이터
+        },
+        state: {
+            showDate: this.thisMonth(1),
+            message: "",
+            startingDayOfWeek: 0,
+            disablePast: false,
+            disableFuture: false,
+            displayPeriodUom: "month",
+            displayPeriodCount: 1,
+            displayWeekNumbers: false,
+            newItemTitle: "",
+            newItemStartDate: "",
+            newItemEndDate: "",
+            useDefaultTheme: true,
+            useHolidayTheme: true,
+            useTodayIcons: "-",
+            items: [
+                {
+                    id: "e1",
+                    startDate: this.thisMonth(15, 18, 30),
+                    title: "그냥"
+                },
+                // {
+                //     id: "e2",
+                //     startDate: this.thisMonth(15),
+                //     title: "Single-day item with a long title",
+                // },
+                // {
+                //     id: "e3",
+                //     startDate: this.thisMonth(7, 9, 25),
+                //     endDate: this.thisMonth(10, 16, 30),
+                //     title: "Multi-day item with a long title and times",
+                // },
+                //... the rest of your items
+            ],
+        }
     }
+    },
+    computed:{
+        todayDate(){
+            return dayjs();
+        },
+        dueDate(){
+            return dayjs('2024-02-07');
+        },
+        dDay(){
+            return this.dueDate.diff(this.todayDate, 'day');
+        },
+        themeClasses(){
+            return{
+                "theme-default": this.state.useDefaultTheme
+            }
+        },
     },
     methods: {
         modalOpen() {
@@ -547,154 +605,106 @@ export default {
         modalOpen2() {
             this.modalCheck2 = !this.modalCheck2
         },
+
+        thisMonth(d, h = 0, m = 0) {
+            const t = new Date()
+            return new Date(t.getFullYear(), t.getMonth(), d, h, m)
+        },
+
+        isoYearMonthDay(date) {
+            const y = date.getFullYear();
+            const m = date.getMonth() + 1; // getMonth()는 0부터 시작하므로 1을 더해줍니다.
+            const d = date.getDate();
+            return `${y}-${m < 10 ? '0' + m : m}-${d < 10 ? '0' + d : d}`;
+        },
+
+        myDateClasses() {
+            const o = {}
+            const theFirst = this.thisMonth(1)
+            const ides = [2, 4, 6, 9].includes(theFirst.getMonth()) ? 15 : 13
+            const idesDate = this.thisMonth(ides)
+            o[this.isoYearMonthDay(idesDate)] = ["ides"]
+            o[this.isoYearMonthDay(this.thisMonth(21))] = ["do-you-remember", "the-21st"]
+            return o
+        },
+
+        periodChanged(){
+
+        },
+
+        onClickItem(e){
+            this.state.message = `You clicked: ${e.title}`
+            
+            if(confirm(`<${e.title}> 일정을 삭제하시겠습니까?`) == true){
+                const index = this.state.items.findIndex(item => item.id === e.id);
+                if (index !== -1) {
+                    this.state.items.splice(index, 1);
+                }
+            }
+        },
+
+        setShowDate(d) {
+            this.state.message = `Changing calendar view to ${d.toLocaleDateString()}`
+            this.state.showDate = d
+        },
+
+        clickTestAddItem() {
+            if (this.state.newItemTitle == "") alert("일정명을 입력해주세요!")
+
+            else{
+                this.state.items.push({
+                    startDate: CalendarMath.fromIsoStringToLocalDate(this.state.newItemStartDate),
+                    endDate: CalendarMath.fromIsoStringToLocalDate(this.state.newItemEndDate),
+                    title: this.state.newItemTitle,
+                    id: "e" + Math.random().toString(36).substring(2, 11),
+                })
+                this.state.message = "You added a calendar item!"
+
+                var el = document.getElementsByClassName('input-text');
+                for(var i=0; i<el.length; i++){
+                    el[i].value = '';
+                }
+            }
+        },
+        
+        saveBtn() {
+            const projectNameInput = document.getElementById('projectname');
+            const teamNameInput = document.getElementById('teamname');
+            //const teamMemberInput = document.getElementById('teammember');
+            const projectStartDateInput = document.getElementById('startdate');
+            const projectEndDateInput = document.getElementById('enddate');
+            const githubLinkInput = document.getElementById('githublink');
+
+            const projectName = projectNameInput.value; // 값을 새로운 변수에 할당합니다.
+            const teamName = teamNameInput.value; // 값을 새로운 변수에 할당합니다.
+            
+            //teamMemberInput.value = '하암';
+            projectStartDateInput.value = this.newProjectStartDate;
+            projectEndDateInput.value = this.newProjectEndDate;
+            githubLinkInput.value = '흠';
+
+            console.log(projectName); //변경하는 이름
+            console.log(teamName); //이것도
+        },
+
+        onclickgithub() {
+            window.open('https://github.com/springvuet-front/')
+        }
     },
+    mounted() {
+        this.state.newItemStartDate = this.isoYearMonthDay(new Date())
+        this.state.newItemEndDate = this.isoYearMonthDay(new Date())
+        this.state.newItemProjectStartDate = this.isoYearMonthDay(new Date())
+        this.state.newItemProjectEndDate = this.isoYearMonthDay(new Date())
+
+        if (this.$refs.bar) {
+            const dDaywidth = this.$refs.bar
+            dDaywidth.style.width = this.dDay * 5 + 'px'
+        }
+    },
+
     components: {
-        LeftMenu, ButtonComponent
+        LeftMenu, ButtonComponent, CalendarView, CalendarViewHeader,
     }
-}
-</script>
-
-<script setup>
-import { onMounted, reactive, computed } from "vue"
-const todayDate = dayjs();
-const dueDate = dayjs('2024-02-07');
-
-const thisMonth = (d, h = 0, m = 0) => {
-    const t = new Date()
-    return new Date(t.getFullYear(), t.getMonth(), d, h, m)
-}
-
-const project = ({
-    exProjectName: '개발 전용 웹 페이지 만들기리기리기억되리',
-    exTeamName: '스프링뷰트',
-    newProjectStartDate: '',
-    newProjectEndDate: '',
-    ProjectStartDate: '2024-01-02', // 프로젝트 시작일 데이터
-    ProjectEndDate: '2024-02-07', // 프로젝트 마감일 데이터
-    dDay: dueDate.diff(todayDate, 'day'),
-})
-
-if (document.getElementById('bar')) {
-  var dDaywidth = document.getElementById('bar')
-  dDaywidth.style.width = project.dDay * 5 + 'px'
-}
-
-const state = reactive({
-    showDate: thisMonth(1),
-    message: "",
-    startingDayOfWeek: 0,
-    disablePast: false,
-    disableFuture: false,
-    displayPeriodUom: "month",
-    displayPeriodCount: 1,
-    displayWeekNumbers: false,
-    showTimes: true,
-    newItemTitle: "",
-    newItemStartDate: "",
-    newItemEndDate: "",
-    useDefaultTheme: true,
-    useHolidayTheme: true,
-    useTodayIcons: false,
-    items: [
-         {
-             id: "e1",
-             startDate: thisMonth(15, 18, 30),
-             title: "그냥"
-         },
-        // {
-        //     id: "e2",
-        //     startDate: thisMonth(15),
-        //     title: "Single-day item with a long title",
-        // },
-        // {
-        //     id: "e3",
-        //     startDate: thisMonth(7, 9, 25),
-        //     endDate: thisMonth(10, 16, 30),
-        //     title: "Multi-day item with a long title and times",
-        // },
-        //... the rest of your items
-    ],
-})
-
-const themeClasses = computed(() => ({
-    "theme-default": state.useDefaultTheme,
-}))
-
-const myDateClasses = () => {
-    const o = {}
-    const theFirst = thisMonth(1)
-    const ides = [2, 4, 6, 9].includes(theFirst.getMonth()) ? 15 : 13
-    const idesDate = thisMonth(ides)
-    o[CalendarMath.isoYearMonthDay(idesDate)] = ["ides"]
-    o[CalendarMath.isoYearMonthDay(thisMonth(21))] = ["do-you-remember", "the-21st"]
-    return o
-}
-
-onMounted(() => {
-    state.newItemStartDate = CalendarMath.isoYearMonthDay(CalendarMath.today())
-    state.newItemEndDate = CalendarMath.isoYearMonthDay(CalendarMath.today())
-    state.newItemProjectStartDate = CalendarMath.isoYearMonthDay(CalendarMath.today())
-    state.newItemProjectEndDate = CalendarMath.isoYearMonthDay(CalendarMath.today())
-})
-
-const periodChanged = () => {}
-
-const onClickItem = (e) => {
-    state.message = `You clicked: ${e.title}`
-    
-    if(confirm(`<${e.title}> 일정을 삭제하시겠습니까?`) == true){
-        const index = state.items.findIndex(item => item.id === e.id);
-        if (index !== -1) {
-            state.items.splice(index, 1);
-        }
-    }
-}
-
-const setShowDate = (d) => {
-    state.message = `Changing calendar view to ${d.toLocaleDateString()}`
-    state.showDate = d
-}
-
-const clickTestAddItem = () => {
-    if (state.newItemTitle == "") alert("일정명을 입력해주세요!")
-
-    else{
-        state.items.push({
-            startDate: CalendarMath.fromIsoStringToLocalDate(state.newItemStartDate),
-            endDate: CalendarMath.fromIsoStringToLocalDate(state.newItemEndDate),
-            title: state.newItemTitle,
-            id: "e" + Math.random().toString(36).substring(2, 11),
-        })
-        state.message = "You added a calendar item!"
-
-        var el = document.getElementsByClassName('input-text');
-        for(var i=0; i<el.length; i++){
-            el[i].value = '';
-        }
-    }
-}
-
-const saveBtn = () => {
-    const projectNameInput = document.getElementById('projectname');
-    const teamNameInput = document.getElementById('teamname');
-    //const teamMemberInput = document.getElementById('teammember');
-    const projectStartDateInput = document.getElementById('startdate');
-    const projectEndDateInput = document.getElementById('enddate');
-    const githubLinkInput = document.getElementById('githublink');
-
-    const projectName = projectNameInput.value; // 값을 새로운 변수에 할당합니다.
-    const teamName = teamNameInput.value; // 값을 새로운 변수에 할당합니다.
-    
-    //teamMemberInput.value = '하암';
-    projectStartDateInput.value = this.newProjectStartDate;
-    projectEndDateInput.value = this.newProjectEndDate;
-    githubLinkInput.value = '흠';
-
-    console.log(projectName); //변경하는 이름
-    console.log(teamName); //이것도
-}
-
-const onclickgithub = () => {
-    window.open('https://github.com/springvuet-front/')
 }
 </script>
