dce8e107e09a2e973b06ddc6b7291f9b7eda72d0
김세희 <lolry0207@sookmyung.ac.kr>
2024-02-01 01:01:04 +0900

    Add Teammember Field

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  //outputDir: "../", //springboot 프로젝트의 resource/stattic 으로 경로 설정
+
+  devServer: {
+    proxy: {
+      '/': {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+      }
+    }
+  }
 })
 module.exports = {
   chainWebpack: config => {
