ea05b116ec91fa4bc8baef51c982d9cfef184280
youngseo <ysnoh22@sookmyung.ac.kr>
2024-02-02 00:00:38 +0900

    community update3

--- src/views/CommunityView.vue
+++ src/views/CommunityView.vue
@@ -148,14 +148,15 @@
                       </div>
                       <div>
                         <input type="text" id="write-comment" value="댓글 작성하기" class="input-long"/>
+                        <div class="send-icon">
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-1 h-1">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+                          </svg>
+                        </div>
                       </div>
                     </div>
 
-                    <!--<div class="send-icon">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-1 h-1">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
-                      </svg>
-                    </div>-->
+                    
 
                   </div>
                 </div>
@@ -413,7 +414,9 @@
 }
 .send-icon {
   background-color: #B1B2FF;
-  size: 15% 15%;
+  width: 35px;
+  height: 35px;
+  transform: rotate(-90deg);
 }
 .comment-icon{
   width: 35px;
