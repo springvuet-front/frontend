90ba5e7bb2cadb478eb6b4bb280da21a6b4f9ac8
JinEunbeen <jay211@sookmyung.ac.kr>
2024-02-06 01:55:27 +0900

    modify todolist Style and try solve Conflict

--- src/views/MypageView.vue
+++ src/views/MypageView.vue
@@ -106,313 +106,314 @@
 </template>
 
 <style scoped>
-.container{
-  padding: 20px 90px;
-}
-#wrap {
-  display: flex;
-  justify-content: center;
-  
-}
-.b_left {
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: center;
-  margin-top: 20px;
-  margin-left: 100px;
-  width: 400px;
-  height: 570px;
-  /* background-color: gray; */
-}
-@media(max-width:1000px){
-  .b_left{
-    display:none;
+  .container{
+    padding: 20px 90px;
+    height: 100vh;
   }
-  .b_right{
+  #wrap {
+    display: flex;
+    justify-content: center;
+  }
+  .b_left {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
     margin-left: 100px;
+    width: 40vw;
+    height: 100vh;
+    /* background-color: gray; */
   }
-}
-.b_right {
-  /* background-color: lightgray; */
-  flex-direction: column;
-  justify-content: center;
-  margin-top: 20px;
-  width: 1200px;
-  height: 570px;
-}
-.schedule {
-  width: 250px;
-  margin-top: 1vw;
-  height: 270px;
-  /* background-color: black; */
-  display: flex;
-  flex-direction: column;
-}
-.toDoList {
-  width: 250px;
-  margin: 10px;
-  height: 270px;
-  display:flex;
-  flex-direction: column;
-  /* background-color: black; */
-}
-
-.listBox {
-  background-color: #F1F1F1;
-  width: 250px;
-  height: 200px;
-  border-radius: 10px;
-  display: flex;
-  align-items: flex-start;
-  overflow-y: auto;
-  padding-top: 5px;
-}
-
-.toDoList .listBox {
-  height: 400px;
-  overflow-x: hidden;
-  flex-wrap: wrap;
-}
-
-.listBox li {
-  font-size: 14pt;
-  line-height: 1.7;
-}
-
-.listBox li::marker {
-  content: '■ ';
-  color: #B1B2FF;
-}
-
-.title {
-  font-size: 19pt;
-  text-align: left;
-  font-weight: 900;
-  padding: 10px;
-  font-family: sans-serif;
-}
-
-.bproject {
-  background-color: white;
-  /* min-height: 45%;s */
-  margin: 10px;
-  height: 270px;
-}
-.bproject.current {
-  display: block;
-}
-.bproject .title {
-  padding-left: 20pt;
-  padding-top: 10pt;
-}
-
-.projectlist {
-  /* background-color: #F2F2F2; */
-  width: 850px;
-  height: 200px;
-  display: flex;
-  flex-wrap: nowrap;
-  /* justify-content: center; */
-  justify-content: space-between;
-}
-
-.projectlist:hover{
-  cursor: pointer;
-}
-
-.project-item {
-  margin-right: 10px; 
-}
-
-.showPastbtn{
-  background-color: rgba(0,0,0,0);
-  font-size: 20pt;
-  color: gray;
-  border: none;
-}
-.showPastbtn:active{
-  color: #B1B2FF;
-  border: none;
-}
-.showNextbtn{
-  background-color: rgba(0,0,0,0);
-  font-size: 20pt;
-  color: gray;
-  border: none;
-
-}
-.showNextbtn:active{
-  color: #B1B2FF;
-  border: none;
-}
-
-/* ProjectBox */
-.projectBox {
+  @media(max-width:1000px){
+    .b_left{
+      display:none;
+    }
+    .b_right{
+      margin-left: 100px;
+    }
+  }
+  .b_right {
+    /* background-color: lightgray; */
+    flex-direction: column;
+    justify-content: center;
+    height: 100vh;
+  }
+  .schedule {
+    width: 250px;
+    margin-top: 1vw;
+    height: 45vh;
+    /* background-color: black; */
+    display: flex;
+    flex-direction: column;
+  }
+  .toDoList {
+    width: 250px;
+    margin: 10px;
+    height: 45vh;
+    display:flex;
+    flex-direction: column;
+    /* background-color: black; */
+  }
+  
+  .listBox {
+    background-color: #F1F1F1;
+    width: 250px;
+    height: 200px;
+    border-radius: 10px;
+    display: flex;
+    align-items: flex-start;
+    overflow-y: auto;
+    padding-top: 5px;
+  }
+  
+  .toDoList .listBox {
+    height: 400px;
+    overflow-x: hidden;
+    flex-wrap: wrap;
+  }
+  
+  .listBox li {
+    font-size: 14pt;
+    line-height: 1.7;
+  }
+  
+  .listBox li::marker {
+    content: '■ ';
+    color: #B1B2FF;
+  }
+  
+  .title {
+    font-size: 19pt;
+    text-align: left;
+    font-weight: 900;
+    padding: 10px;
+    font-family: sans-serif;
+  }
+  
+  .bproject {
+    background-color: white;
+    /* min-height: 45%;s */
+    margin: 10px;
+    height: 270px;
+  }
+  .bproject.current {
+    display: block;
+    height: 45vh;
+  }
+  .bproject.complete{
+    height: 45vh;
+  }
+  .bproject .title {
+    padding-left: 20pt;
+    padding-top: 10pt;
+  }
+  
+  .projectlist {
+    /* background-color: #F2F2F2; */
+    width: 850px;
+    height: 200px;
+    display: flex;
+    flex-wrap: nowrap;
+    /* justify-content: center; */
+    justify-content: space-between;
+  }
+  
+  .current .projectlist .projectlist:hover{
+    cursor: pointer;
+  }
+  
+  .project-item {
+    margin-right: 10px; 
+  }
+  
+  .showPastbtn{
+    background-color: rgba(0,0,0,0);
+    font-size: 20pt;
+    color: gray;
+    border: none;
+  }
+  .showPastbtn:active{
+    color: #B1B2FF;
+    border: none;
+  }
+  .showNextbtn{
+    background-color: rgba(0,0,0,0);
+    font-size: 20pt;
+    color: gray;
+    border: none;
+  
+  }
+  .showNextbtn:active{
+    color: #B1B2FF;
+    border: none;
+  }
+  
+  /* ProjectBox */
+  .projectBox {
+      width: 200px;
+      height: 200px;
+      background-color: #D2DAFF;
+      border-radius: 15px;
+      display: block;
+      font-size: 12pt;
+      margin: 20px;
+      margin-top: 0px;
+  }
+  .current .projectlist .projectBox:hover{
+      box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+  }
+  .project.category {
+      width: 150px;
+      height: 50px;
+      display: flex;
+  }
+  .project.field {
+      border-radius: 50%;
+      background-color: #B1B2FF;
+      margin-top: 10px;
+      margin-left: 10px;
+      width: auto;
+      min-width: 30px;
+      height: 30px;
+      text-align: center;
+      line-height: 30px;
+  }
+  .project.myPart {
+      border-radius: 10px;
+      border: solid;
+      border-color: #B1B2FF;
+      width: auto;
+      min-width: 27px;
+      height: 27px;
+      text-align: center;
+      line-height: 27px;
+      margin-top: 10px;
+      margin-left: 10px;
+  }
+  .project.info{
+      width: 200px;
+      height: 150px;
+      background-color: #B1B2FF;
+      border-bottom-left-radius: 15px;
+      border-bottom-right-radius: 15px;
+      font-size: 10pt;
+      text-indent: 7px;
+      line-height: 1.5;
+      color: white;
+      overflow: hidden;
+      justify-content: center;
+  }
+  .project.name{
+    padding-top: 10px;
+    font-size: 17pt;
+    font-weight: 900;
+    white-space: wrap;
+  }
+  
+  .project.team{
+    font-size: 13pt;
+    font-weight: 600;
+  }
+  
+  .project.date{
+    line-height: 1.5;
+  }
+  
+  /* toDoList 스타일*/
+  .inputToDoList {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-bottom: 5px;
+  }
+  .inputToDoList input[type="text"]{
+    width: 180px;
+    height: 30px;
+    border: solid;
+    border-bottom-left-radius: 7px;
+    border-top-left-radius: 7px;
+    font-size: 14pt;
+    background-color: white;
+    margin-right: 0px;
+    margin-left: 10px;
+    margin-top: 5px;
+  }
+  
+  .plusbtn{
+    width: 36px;
+    height: 36px;
+    font-size: 20pt;
+    font-weight: 600;
+    flex-direction: column;  
+    align-items: center; 
+    justify-content: center;
+    background-color: rgb(0,0,0,0);
+    border: solid;
+    border-left: 0;
+    border-bottom-right-radius: 7px;
+    border-top-right-radius: 7px;
+    /* float: right; */
+    margin-top: 5px;
+  }
+  
+  .checkToDoList{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+  .checkToDoList input[type="checkbox"]{
+    margin-left: 10px;
+    width: 25px;
+    height: 25px;
+  }
+  .checkToDoList input[type="text"]{
+    width: 190px;
+    min-height: 30px;
+    border: 0;
+    font-size: 14pt;
+    background-color: #F1f1F1
+  }
+  .deleteToDoBtn{
+    position:relative;
+    left: -40px;
+    width: 25px;
+    height: 25px;
+    border: 0;
+    background-color: #F1f1F1;
+    font-size: 20px;
+    color: rgb(0,0,0,0.3);
+  }
+  .checkToDoList input[type="text"]:focus{
+    outline: 0;
+  }
+  .checkToDoList input[type="text"]:disabled{
+    text-decoration: line-through;
+  }
+  .deleteToDoBtn:disabled{
+    visibility:hidden;
+  }
+  /* addProjectBtn */
+  
+  .addNewBtnStyle:hover{
+    box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+  }
+  .addProjectBtn {
     width: 200px;
     height: 200px;
     background-color: #D2DAFF;
     border-radius: 15px;
     display: block;
     font-size: 12pt;
-    margin: 20px;
-    margin-top: 0px;
-}
-.projectBox:hover{
-    box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
-}
-.project.category {
-    width: 150px;
+    border: 0;
+  }
+  .addProjectBtn:hover{
+      box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+  }
+  
+  .showbtnimg{
+    width: 30px;
     height: 50px;
-    display: flex;
-}
-.project.field {
-    border-radius: 50%;
-    background-color: #B1B2FF;
-    margin-top: 10px;
-    margin-left: 10px;
-    width: auto;
-    min-width: 30px;
-    height: 30px;
-    text-align: center;
-    line-height: 30px;
-}
-.project.myPart {
-    border-radius: 10px;
-    border: solid;
-    border-color: #B1B2FF;
-    width: auto;
-    min-width: 27px;
-    height: 27px;
-    text-align: center;
-    line-height: 27px;
-    margin-top: 10px;
-    margin-left: 10px;
-}
-.project.info{
-    width: 200px;
-    height: 150px;
-    background-color: #B1B2FF;
-    border-bottom-left-radius: 15px;
-    border-bottom-right-radius: 15px;
-    font-size: 10pt;
-    text-indent: 7px;
-    line-height: 1.5;
-    color: white;
-    overflow: hidden;
-    justify-content: center;
-}
-.project.name{
-  padding-top: 10px;
-  font-size: 17pt;
-  font-weight: 900;
-  white-space: wrap;
-}
-
-.project.team{
-  font-size: 13pt;
-  font-weight: 600;
-}
-
-.project.date{
-  line-height: 1.5;
-}
-
-/* toDoList 스타일*/
-.inputToDoList {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  margin-bottom: 5px;
-}
-.inputToDoList input[type="text"]{
-  width: 180px;
-  height: 30px;
-  border: solid;
-  border-bottom-left-radius: 7px;
-  border-top-left-radius: 7px;
-  font-size: 14pt;
-  background-color: white;
-  margin-right: 0px;
-  margin-left: 10px;
-  margin-top: 5px;
-}
-
-.plusbtn{
-  width: 36px;
-  height: 36px;
-  font-size: 20pt;
-  font-weight: 600;
-  flex-direction: column;  
-  align-items: center; 
-  justify-content: center;
-  background-color: rgb(0,0,0,0);
-  border: solid;
-  border-left: 0;
-  border-bottom-right-radius: 7px;
-  border-top-right-radius: 7px;
-  /* float: right; */
-  margin-top: 5px;
-}
-
-.checkToDoList{
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-}
-.checkToDoList input[type="checkbox"]{
-  margin-left: 10px;
-  width: 25px;
-  height: 25px;
-}
-.checkToDoList input[type="text"]{
-  width: 190px;
-  min-height: 30px;
-  border: 0;
-  font-size: 14pt;
-  background-color: #F1f1F1
-}
-.deleteToDoBtn{
-  position:relative;
-  left: -40px;
-  width: 25px;
-  height: 25px;
-  border: 0;
-  background-color: #F1f1F1;
-  font-size: 20px;
-  color: rgb(0,0,0,0.3);
-}
-.checkToDoList input[type="text"]:focus{
-  outline: 0;
-}
-.checkToDoList input[type="text"]:disabled{
-  text-decoration: line-through;
-}
-.deleteToDoBtn:disabled{
-  visibility:hidden;
-}
-/* addProjectBtn */
-
-.addNewBtnStyle:hover{
-  box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
-}
-.addProjectBtn {
-  width: 200px;
-  height: 200px;
-  background-color: #D2DAFF;
-  border-radius: 15px;
-  display: block;
-  font-size: 12pt;
-  border: 0;
-}
-.addProjectBtn:hover{
-    box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
-}
-
-.showbtnimg{
-  width: 30px;
-  height: 50px;
-}
+  }
 
 </style>
     
